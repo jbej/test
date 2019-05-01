400eb522d5f7d5f762e548ca0167e4ca3600a1dc
Justyna Bejgrowicz <justyna.bejgrowicz@interia.pl>
2019-05-01 15:49:44 +0300

    Updates

---
+++ precache-manifest.abcb00c612e4ced9bb70b7681f1119a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23fd1cd65a8563fa95bd",
+    "url": "/weather_app/static/js/runtime~main.17b50636.js"
+  },
+  {
+    "revision": "d63625ce989c0cddc38b",
+    "url": "/weather_app/static/js/main.57dc3b2d.chunk.js"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/weather_app/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "d63625ce989c0cddc38b",
+    "url": "/weather_app/static/css/main.c3753f59.chunk.css"
+  },
+  {
+    "revision": "c6154fd96fa885fe6996b0d043bb328d",
+    "url": "/weather_app/index.html"
+  }
+];
